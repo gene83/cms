f865398b92bdacc6e57fae50d81ef5a306781269
Gene Hall <genehall@eworldes.com>
2023-05-10 14:26:53 -1000

    setup test button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState("");
+
+  function fetchData() {
+    setData("FETCHED")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CMS</h1>
       </header>
+      <div>
+        <button onClick={() => fetchData()}>
+          TEST
+        </button>
+        {data}
+      </div>
     </div>
   );
 }
