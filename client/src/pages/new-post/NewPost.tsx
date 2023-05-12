5b4fd8472f35f521d8e5439ee45c321665fcc93a
Gene Hall <genehall@eworldes.com>
2023-05-11 15:18:52 -1000

    drag and drop working for multiple images

--- client/src/pages/new-post/NewPost.tsx
+++ client/src/pages/new-post/NewPost.tsx
@@ -1,14 +1,60 @@
 import { FaChevronDown } from 'react-icons/fa';
+import { useDropzone } from 'react-dropzone';
 import './NewPost.scss';
+import { useCallback, useState } from 'react';
 
 type Category = '' | 'Vehicles' | 'Sporting Goods' | 'Clothing';
 const CATEGORIES: Category[]  = ['', 'Vehicles', 'Sporting Goods', 'Clothing'];
 
+type Image = File & { id: number, src: string }
+
+
 const NewPost = () => {
+    const [images, setImages] = useState<Image[]>([]);
+
+    const onDrop = useCallback((files: File[]) => {
+        files.map((file, index) => {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                setImages((prevState) => [
+                    ...prevState,
+                    { id: index, src: e.target?.result } as Image
+                ])
+            }
+            reader.readAsDataURL(file);
+            return file;
+        })
+    }, []); 
+
+    const {getRootProps, getInputProps} = useDropzone({
+        accept: {
+            'image/jpeg': ['.jpeg', '.jpg'], 
+            'image/png': ['.png'] 
+        },
+        maxFiles: 6,
+        onDrop,
+    });
+    
+
+    const previews = images.map((image) => (
+        <div key={image.id}>
+            <img src={image.src} alt={image.id.toString()} />
+        </div>
+    ))
 
     return (
         <div className="post-container">
             <h1>New Post</h1>
+            <section className="container">
+                <div {...getRootProps({ className: 'dropzone' })}>
+                    <input {...getInputProps()} />
+                    <p>Drag 'n' drop here, or click to select files</p>
+                </div>
+                <aside>
+                    <h4>Files</h4>
+                    <ul>{previews}</ul>
+                </aside>
+            </section>
             <form onSubmit={(e) => {
                 e.preventDefault();
                 const formData = new FormData(e.currentTarget);
